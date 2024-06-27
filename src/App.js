7392a47050ff0406df3ff4d71f7b9073e0589122
nileshk16 <nileshkumark162@gmail.com>
2024-06-27 20:35:19 +0530

    Sematic HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Main from './components/Main';
+
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <img src="logo.svg"></img>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>Order Online</a></li>
+            <li><a>Login</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
